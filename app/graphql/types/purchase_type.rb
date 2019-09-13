0cea3c006a9768479418cf10917556efe02cae2f
isalevine <isa.loves.coding@gmail.com>
2019-09-12 21:00:28 -0700

    adding custom graphql fields as methods

--- app/graphql/types/purchase_type.rb
+++ app/graphql/types/purchase_type.rb
@@ -4,9 +4,10 @@ module Types
     field :username, String, null: false
     field :price, Float, null: false
     field :transactions, [Types::TransactionType], null: true
+    field :transactions_count, Integer, null: true
 
-    # def transactions_count
-    #   object.transactions.count
-    # end
+    def transactions_count
+      object.transactions.size
+    end
   end
 end
