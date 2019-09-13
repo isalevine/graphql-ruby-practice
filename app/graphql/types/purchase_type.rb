697b298a18751f985609387ad5cbc2021f5e82aa
isalevine <isa.loves.coding@gmail.com>
2019-09-12 21:06:05 -0700

    now calculating current balance in graphql

--- app/graphql/types/purchase_type.rb
+++ app/graphql/types/purchase_type.rb
@@ -5,9 +5,21 @@ module Types
     field :price, Float, null: false
     field :transactions, [Types::TransactionType], null: true
     field :transactions_count, Integer, null: true
+    field :current_balance, Float, null: true
 
     def transactions_count
       object.transactions.size
     end
+
+    def current_balance
+      balance = object.price
+      if !object.transactions.empty?
+        object.transactions.each do |transaction|
+          balance -= transaction.amount
+        end
+      end
+      return balance
+    end
+
   end
 end
