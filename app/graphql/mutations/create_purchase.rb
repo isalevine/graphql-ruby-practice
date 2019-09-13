db55164239d5b2dfd0a9bc3038d5338ccbc85c8e
isalevine <isa.loves.coding@gmail.com>
2019-09-12 21:34:21 -0700

    now creating new purchases

--- app/graphql/mutations/create_purchase.rb
+++ app/graphql/mutations/create_purchase.rb
@@ -1,1 +1,29 @@
 # created per tutorial: https://mattboldt.com/2019/01/07/rails-and-graphql/
+
+class Mutations::CreatePurchase < Mutations::BaseMutation
+    argument :username, String, required: true
+    argument :price, Float, required: true
+
+    field :purchase, Types::PurchaseType, null: false
+    field :message, String, null: true
+    field :errors, [String], null: false
+    
+    def resolve(username:, price:)
+        purchase = Purchase.new(username: username, price: price)
+        if purchase.save
+            {
+                purchase: purchase,
+                message: "Purchase created successfully!",
+                errors: []
+            }
+        else
+            {
+                purchase: nil,
+                message: "Purchase NOT created!! See errors!",
+                errors: purchase.errors.full_messages
+            }
+        end
+    end
+
+
+end
--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,5 +1,10 @@
 module Types
   class MutationType < Types::BaseObject
+
+
+    field :create_purchase, mutation: Mutations::CreatePurchase
+
+
     # # TODO: remove me
     # field :test_field, String, null: false,
     #   description: "An example field added by the generator"
